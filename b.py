9588fc5d9ed384097c185af72f2d96d8a8b3ce11
PrismaticPolygon <domwhite97@gmail.com>
2020-02-12 18:25:58 +0000

    Finished bitchez

--- b.py
+++ b.py
@@ -5,10 +5,7 @@ teachers = etree.parse("xml/teachers.xml")
 
 print("Find all students who study Advanced Databases this year.\n")
 
-path = "/students[@year='2019-2020']/student/course/submodule[text()='Advanced Databases']/ancestor::student"
-
-# This returns the submodule. We now what to get the student.
-
+path = "/students/descendant::submodule[text()='Advanced Databases']/ancestor::student"
 result = students.xpath(path)
 
 for r in result:
@@ -17,24 +14,18 @@ for r in result:
 
 print("\nFind all teachers who teach Advanced Databases this year.\n")
 
-# path = "/teachers/teacher[course[submodule[name='Advanced Databases' and year='2019-2020']]]"
-
-path = "/teachers/teacher/course/submodule[name='Advanced Databases' and year='2019-2020']/ancestor::teacher"
-
+path = "/teachers/descendant::submodule[name='Advanced Databases' and year='2019-2020']/ancestor::teacher"
 result = teachers.xpath(path)
 
 for r in result:
 
     print(etree.tostring(r, pretty_print=True))
 
-# c) How many years has Professor Cristea been teaching ‘Advanced Databases’ (at Durham)?
+print("\nc) How many years has Professor Cristea been teaching ‘Advanced Databases’ (at Durham)?\n")
 
-query = """
-    for $x in doc("xml/teachers.xml")/teachers/teacher
-    where $x/name=='Alexandra Cristea'
-    return 2020 - $x/joiningDate
-"""
+path = "count(/teachers/teacher[name='Alexandra Cristea']/course/submodule[name='Advanced Databases']/year)"
+result = teachers.xpath(path)
 
-# d) Find all students n year 3 currently taught by Alexandra.
+print(result)
 
-# e) How many teachers and how many students are kept in the databases where the last name is not known?
+
