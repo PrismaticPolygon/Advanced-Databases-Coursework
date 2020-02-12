367887cf24f64f5d9b50c74aa3d772e1e0cbde46
PrismaticPolygon <domwhite97@gmail.com>
2020-02-12 15:28:09 +0000

    Finished B.a, B.b

--- b.py
+++ b.py
@@ -5,16 +5,22 @@ teachers = etree.parse("xml/teachers.xml")
 
 print("Find all students who study Advanced Databases this year.\n")
 
-path = "/students[@year='2019-2020'][//student//courses//]"
-# result = students.xpath(path)
-#
-# for r in result:
-#
-#     print(etree.tostring(r, pretty_print=True))
+path = "/students[@year='2019-2020']/student/course/submodule[text()='Advanced Databases']/ancestor::student"
+
+# This returns the submodule. We now what to get the student.
+
+result = students.xpath(path)
+
+for r in result:
+
+    print(etree.tostring(r, pretty_print=True))
 
 print("\nFind all teachers who teach Advanced Databases this year.\n")
 
-path = "/teachers/teacher[course[submodule[name='Advanced Databases' and year='2019-2020']]]"
+# path = "/teachers/teacher[course[submodule[name='Advanced Databases' and year='2019-2020']]]"
+
+path = "/teachers/teacher/course/submodule[name='Advanced Databases' and year='2019-2020']/ancestor::teacher"
+
 result = teachers.xpath(path)
 
 for r in result:
